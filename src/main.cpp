0e4f844b8f47b3a657396b2cacd13f06f49d409f
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2022-12-12 22:25:59 +0100

    Added windows hide console

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include "vk_engine.h"
+#include <iostream>
+
+#if (defined(WIN32) || defined(WIN64) || defined(_WIN32) || \
+     defined(_WIN64)) &&                                    \
+    defined(NDEBUG)
+#include <windows.h>
+#endif
 
 int main(int argc, char *argv[]) {
+#if (defined(WIN32) || defined(WIN64) || defined(_WIN32) || \
+     defined(_WIN64)) &&                                    \
+    defined(NDEBUG)
+  HWND windowHandle = GetConsoleWindow();
+  ShowWindow(windowHandle, SW_HIDE);
+#endif
+
   VulkanEngine engine;
 
   engine.init();
