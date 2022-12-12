918236d114f32af44e0b67bada830e1722d2ad1f
K-U-R-C <jak.kurc@gmail.com>
2022-12-12 21:58:59 +0100

    Added Windows MSVC hide console in release mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,6 @@
 #include "vk_engine.h"
-#include <iostream>
-
-#if (defined(WIN32) || defined(WIN64) || defined(_WIN32) || \
-     defined(_WIN64)) &&                                    \
-    defined(NDEBUG)
-#include <windows.h>
-#endif
 
 int main(int argc, char *argv[]) {
-#if (defined(WIN32) || defined(WIN64) || defined(_WIN32) || \
-     defined(_WIN64)) &&                                    \
-    defined(NDEBUG)
-  HWND windowHandle = GetConsoleWindow();
-  ShowWindow(windowHandle, SW_HIDE);
-#endif
-
   VulkanEngine engine;
 
   engine.init();
